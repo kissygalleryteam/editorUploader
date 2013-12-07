daec9f3d5c4d991f682c28065c005c1beac052c0
jianping.xwh <jianping.xwh@taobao.com>
2013-12-07 16:24:45 +0800

    发布v1.4

---
+++ 1.4/build/index.js
@@ -0,0 +1,60 @@
+/*
+combined files : 
+
+gallery/editorUploader/1.4/index
+
+*/
+/**
+ * multipleUpload button
+ * @author yiminghe@gmail.com
+ */
+KISSY.add('gallery/editorUploader/1.4/index',function (S, Editor, DialogLoader) {
+    S.config('modules',{
+        "editor/plugin/editor-upload/dialog": {alias: ['gallery/editorUploader/1.4/dialog']}
+    });
+    function multipleUpload(config) {
+        if(!config.tpl){
+            config.tpl = '<div class="grid" style="margin:10px;"> ' +
+                    '<input type="file" class="g-u" id="{{prefix}}uploader-button" value="上传图片" name="imgFile"  accept="image/*" > ' +
+                    '<input type="hidden" id="J_Urls" name="urls" value="" />' +
+                ' </div>' +
+                ' <div class="editor-uploader-queue-wrapper J_UploaderQueueWrapper" style="width:600px;margin-top: 20px;">' +
+                     ' <table class="ks-editor-upload-list" width="100%" border="0" border-spacing="0" border-collapse="collapse">' +
+                         ' <thead> ' +
+                             '<tr> <th>图片</th> <th>大小</th> <th style="width:30%">上传状态</th> <th>图片操作</th> </tr> ' +
+                         '</thead>' +
+                         ' <tbody class="{{prefix}}uploader-queue"> </tbody>' +
+                     ' </table> ' +
+                     '<div class="uploader-footer">' +
+                         ' <a class="ks-editor-button ks-editor-multiple-upload-ok ks-inline-block J_StartUpload">开始上传</a> ' +
+                         '<a class="ks-editor-button ks-editor-multiple-upload-insertall ks-inline-block J_InsertContent" style="margin-left:20px;">全部插入</a>' +
+                         ' <a class="ks-editor-uploader-clear J_ClearQueue">清空列表</a>' +
+                '    </div>' +
+                ' </div>';
+        }
+        this.config = config || {};
+    }
+
+    S.augment(multipleUpload, {
+        pluginRenderUI:function (editor) {
+            var self = this;
+            var button = editor.addButton("multipleUpload", {
+                tooltip:"批量插图",
+                listeners:{
+                    click:function () {
+                        DialogLoader.useDialog(editor, "editor-upload", self.config);
+                    }
+                },
+                mode:Editor.WYSIWYG_MODE
+            });
+            var $el = button.$el;
+            $el.children().addClass('ks-editor-toolbar-image');
+        }
+    });
+
+    return multipleUpload;
+
+
+}, {
+    requires:['editor', 'editor/plugin/dialog-loader']
+});
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 ## editorUploader
+kissy editor的图片上传由于是使用flash上传，很有可能会出现session丢失的bug，所以写了这个插件用于替换editor的上传插件。
 
-* 版本：1.0
+v1.4基于kissy1.4，使用[uploader1.5](http://gallery.kissyui.com/uploader/1.5/guide/index.html)。
+
+
+* 版本：1.4
 * 作者：明河
+
+## 组件变更
+
+### V1.4 change
+
+    [!]基于kissy1.4
+    [!]基于uploader1.5
+    [!]使用通用接口，简化配置
+    [!]优化代码
 
 
