3509eb85aeaf544d5fc7fb9a2265c42ae252973f
kissyteam <tiehang.lth@taobao.com>
2015-01-23 05:04:48 +0800

    sync daily branch 2.0.3

--- 1.4/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'gallery/editorUploader/index': {alias: ['gallery/editorUploader/1.0/index']}
-});
--- 2.0.2/build/mods.js
+++
@@ -1,3 +0,0 @@
-KISSY.config('modules', {
- 'kg/editoruploader/2.0.1/index': { requires: ['editor', 'editor/plugin/dialog-loader']} 
-});
